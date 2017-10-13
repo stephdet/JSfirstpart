7362c1850f370ab138ef9e5e2e0b890fd95782cf
Unknown <sdethy@gmail.com>
2017-10-13 17:17:58 +0200

    presque fin3

--- exercices.js
+++ exercices.js
@@ -107,13 +107,58 @@ var result =11;
 */
 
 
-var mot="vache";
-var nombre=4;
-var resultat="";
+// var mot="vache";
+// var nombre=4;
+// var resultat="";
+//
+//
+// if (nombre>1) {
+//     resultat=mot +"s";
+//   }
+//
+// console.log(resultat);
 
 
-if (nombre>1) {
-    resultat=mot +"s";
-  }
+//
+// Exercice : Calculs
+//
+// Déclarez les variables suivantes :
+//
+//     Fonction qui fait des soustractions
+//     Fonction qui fait des division
+//     Fonction qui fait des multiplications
+//     Fonction qui fait des calcule de pourcentages ( 2 arguments: valeur et pourcentage ) par exemple : "50% de 10 = 5";
+//
+//     Faire une fonction qui fait des calculs de vitesse (2 arguments : distance, temps en heure)
+//     Le résultat doit être par exemple : '50km/h'
+//
 
-console.log(resultat);
+// var number1 =4;
+// var number2 =5;
+//
+// function Soustarction() {
+//   var result=number1 - number2 ;
+//   return console.log(result);
+// }
+// Soustarction();
+//
+
+
+// var number1 =14;
+// var number2 =7;
+//
+// function Division() {
+//   var result=number1 / number2 ;
+//   return console.log(result);
+// }
+// Division();
+//
+//
+//
+
+var vitesse = geResults(distance,temps){
+   return  var result=distance/temps;
+}
+
+
+console.log(result);
