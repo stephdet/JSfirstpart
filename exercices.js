f7178a15dde43a842642f6be29e9b33bbe02be18
Unknown <sdethy@gmail.com>
2017-10-13 15:30:34 +0200

    fin exercice2

---
+++ exercices.js
@@ -0,0 +1,119 @@
+//EXERCICE 1
+
+//
+// var enfant=5;
+// var nom="Véronique";
+// var pays="Suisse";
+// var metier="journaliste";
+//
+//
+// console.log("Vous serez "+metier+ "et habiterez "+pays+ "et marié "+nom+" avec "+enfant+" enfants ")
+//
+//
+//
+// var year=2017;
+// var myyear=1969;
+// var age=year - myyear;
+//
+// console.log("l'age de la personne est "+age)
+//
+//
+// var monage=47;
+// var agemax=100;
+// var denree="pain";
+// var consomation=2;
+//
+//
+// var timelife=((agemax - monage)*365)*2;
+//
+// console.log("il vous reste "+timelife+ "de "+denree+"avant d'atteindre lagede "+agemax+ "ans")
+//
+// var result = (1 + 2) * 3 + 4 / 2;
+/* parenthespriorit
+var result = 3 * 3 + 4 / 2;
+multiplication
+var result = 9 + 4 / 2;
+division
+var result = 9 + 2;
+addition
+var result =11;
+
+*/
+// //EXERCICE 2
+// var lang="fr";
+// var msg="";
+//
+// var lang=prompt("choose....");
+//
+//
+// if(lang == "fr"){
+//   msg = "Bonjour tout le monde";
+// }
+// else if(lang == "es"){
+//   msg="Holla el mundo";
+// }
+// else if(lang == "uk"){
+//   msg="Hello everybo";
+// }
+//
+//
+// console.log(msg);
+
+
+
+
+/*Exercice : Affichage des scores
+
+    Crée une variable qui contient un score
+    Crée une variable qui contiendra le resultat
+    Faire des conditions pour pour etablir les rang selon le score
+
+    Message :Vous avez le rang A si vous avez un score égal ou supérieur a 90
+    Vous avez le rang B si vous avez un score inférieur a 90
+    Vous avez le rang C si vous avez un score inférieur ou égal a 50
+*/
+
+
+// var score=prompt("choose....");
+// var resultat="";
+//
+//
+// if (score>=90) {
+//   resultat="Vous avez le rang A";
+// }
+//   else if (resultat<90) {
+//     resultat="Vous avez le rang B";
+//   }
+//   else if (resultat<=90) {
+//     resultat="Vous avez le rang C";
+//   }
+//
+//   console.log(resultat);
+//
+//
+//
+
+
+
+
+/*  Exercice : Mettre au pluriel
+
+      Crée une variable contenant un mot au singulier
+      crée une variable qui contient le nombre dont tu en dispose
+      crée une variable qui contiendra le résultat
+      Faire une condition pour savoir si il faut rajouter un "s" ou pas
+
+  Le résultat doit être par exemple : "Je possède 4 pommes"
+*/
+
+
+var mot="vache";
+var nombre=4;
+var resultat="";
+
+
+if (nombre>1) {
+    resultat=mot +"s";
+  }
+
+console.log(resultat);
