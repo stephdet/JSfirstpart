aa813f56671950fb04e34bfbd56389813e63fd1e
Unknown <sdethy@gmail.com>
2017-10-22 22:29:33 +0200

    les modifs du jours

--- 06-JEUXPENDU/exercices.js
+++ 06-JEUXPENDU/exercices.js
@@ -1,16 +1,32 @@
 //EXERCICE 1
 
+
+//     window.prompt();
+//     window.alert();
+//     window.confirm();
+
+
 var mot = ["B","O","N","J","O","U","R"];
 var guessedLetter = ["-","-","-","-","-","-","-"];
 var compteurVictoire =0;
+
+var fenetre=confirm("Voulez-vous jouer au pendu?");
+	if (fenetre == false) {
+		alert("Aurevoir...");
+		if (false) {
+		fenetre.close();
+	}
+	}
+	else {
+
 function guessLetter() {
-  var message=prompt("entrer une letter à deviner");
-  message =message.toUpperCase();
-  console.log(message);
+  var message=prompt("entrer une letter à deviner entre A et Z");
+  var message =message.toUpperCase();
+
   if (message.lenght != 1) {
-    console.log("trop de caratcère"+message.lenght+"caractere");
-    console.log(message.lenght);
-  }else{
+  console.log("Entrez seulementun caractère");
+  }
+  else{
     if (mot.indexOf(message)== -1) {
       console.log("Introuvable");
     }
@@ -20,7 +36,7 @@ function guessLetter() {
             if (message == mot[i]) {
               compteurVictoire ++;
               console.log("yeah ! j'ai trouvé ta lettre à l'index"+i);
-               guessedLetter.splice(i,1,message);
+               guessedLetter.slice(i,1,mot);
                console.log(guessedLetter);
             }
           }
@@ -32,6 +48,8 @@ function guessLetter() {
   else {
   guessLetter();
 //fonction recursive
-}
-}
+  }
 guessLetter();
+  }
+guessLetter();
+}
--- 08-OBJET/exercices.js
+++ 08-OBJET/exercices.js
@@ -1,53 +1,111 @@
 
-  //  La valeur est égal au chiffre le plus petit dans l'enssemble suivant : 7, 5, -12, 6, 32, 18, 14, -2
-  //  La valeur est égal au chiffre le plus grand dans l'enssemble suivant : -3, 9, 21, 36, 27, 54, 17, 35
-  //  La valeur est égal a l'addition des deux précédentes variables
+
+// let Character ={
+// "name":"Stephane",
+// "age":48,
+// }
+// //Mon tableau
+// let items_to_give = ["livre","portable","livre","didactic"];
+// //on va dans crer une fonction pour aller chercher la liste
+// let fullCharater={};
+//   for(let i in Character){
+// fullCharater[i]=Character[i];
+//   }
+// console.log(fullCharater.name);
+// console.log(fullCharater.age);
+
+
+//Exercice 1
+//je crée ma function pour donner une valeur de façon aléatoire
 //
-// var x = Math.min(7,5,-12,6,32,18,14,-2);
-// var y = Math.max(-3,9,21,36,27,54,17,35);
-// var sum =x + y;
-// console.log(x);
-// console.log(y);
-// console.log(sum);
+// function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
+//
+// var Character ={
+//   "name":"Stephane",
+//   "age":48,
+//   items_to_give:["livre","portable","livre","didactic"],
+// //je cree la fonction qui va permettre à lobjet de retourner les valeurs du tableau garace à la function getRndInteger
+// //et avec la profondeur du tablau
+//   giveItem:function () {
+//     return this.items_to_give[getRndInteger(0,this.items_to_give.lenght-1)];
+// },
+// };
 
-
-  //  Faire une variable floatBateau dont la valeur est 10.4
-  //  Faire une variable couleBateau dont la valeur est floatBateau arrondie vers le bas
-  //  Faire une variable voleBateau dont la valeur est floatBateau arrondie vers le haut
-
-
-
-// var floatBateau =10.4;
-// var voleBateau = Math.ceil(floatBateau);
-// var couleBateau = Math.floor(floatBateau);
-
-
-
-
-//Déclarez le  La valeur aléatoire entre 50 et 100
-  //  La valeur aléatoire peut être seulement 0 ou 1
-  //  La valeur aléatoire peut aller de 0 à 10 inclus
-
-// Aleatoire 2
-// function MyNumberarbitrary(min,max) {
-//   return Math.floor(Math.random() * (max - min)) + min;
+// var
+//   for (let i in Character) {
+//     fullCharater[i]=Character[i];
+//   console.log(i);
 // }
 //
 
-/*
-console.log('hello');
-
-var Firstresult = Math.floor(Math.random() *50)+50;
-console.log(Firstresult);
-var Secondresult= Math.round(Math.random());
-console.log(Secondresult);
-var Threeresult=Math.ceil(Math.random()*10);
-console.log(Threeresult);
-*/
+// // Array.prototype.arrCustom = function (){};
+//  let items_to_give=["livre","portable","livre","didactic"];
+// for (let i in items_to_give) {
+//   console.log(items_to_give[i]);
+// }
 
 
+//Exercice 2
+
+//faire le tbleau avec les caractéristiques de chaque objet :
+
+function  ShowAvailableItems() {
+  itemshop.forEach(function(element) {
+  if (element.available) console.log(element.title);
+});
+}
+
+
+var itemshop =[{
+  title:"épée",
+  physic:12,
+  magic:8,
+  minLevel:5,
+  available:true,
+},{
+  title: 'Hache',
+  physic: 30,
+  magic: 0,
+  minLevel: 10,
+  available: false,
+},{
+  title: 'Sceptre',
+  physic: 0,
+  magic: 20,
+  minLevel: 5,
+  available: true,
+}];
 
 
 
-var tab=["Goro", "Johnny Cago", "Kano","Liu Kano"," Raiden", "Reptil", "Scorpion", "Shang Tsun", "Sonya", "Sub-Zero"];
-console.log(tab[Math.floor(Math.random()*tab.length)]);
+
+
+
+
+
+
+
+
+
+// let gun=["épée", "hache","sceptre"];
+//
+//
+// let databody={
+//   "title":"hache";
+//   physic:40;
+//   magic:23;
+//   minLevel:87;
+//   available:true;
+// }
+//
+//
+// //creer une boucle et associer les objets du tableau avec les valeurs de l'objet
+//
+// let gun{};
+//   for(let i in databody){
+//     gun[i]=databody[i];
+//   }
+//   console.log(gun.title);
+// }
